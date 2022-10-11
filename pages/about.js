8a0c09ffd14d742b8e7862df139c53d969477b79
ismayito <47735113+ismayito@users.noreply.github.com>
2022-10-11 18:12:45 +0300

    completed next js  app

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Meta from '../components/Meta'
 
 const about = () => {
   return (
     <div>
+      <Meta title="about"></Meta>
         <h1>about pages</h1>
     </div>
   )
--- pages/article/[id].js
+++ pages/article/[id].js
@@ -3,12 +3,15 @@ import {useRouter} from "next/router"
 import Link from 'next/link';
 import { server } from '../../config';
 import { resolve } from 'styled-jsx/css';
+import Meta from '../../components/Meta';
+
 
 const article = ({article}) => {
     // const router= useRouter();
     // const {id} = router.query
   return (
     <div>
+      <Meta title={article.title} description={article.body}></Meta>
         <h1>{article.title}</h1>
         <p>{article.body}</p>
         <br></br>
