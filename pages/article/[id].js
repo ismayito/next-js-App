cf9502ca88cb2081dc8aa0e713ea2b6076147a12
ismayito <47735113+ismayito@users.noreply.github.com>
2022-10-11 16:09:47 +0300

    using  getServerSideProps to fetch data

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,14 +1,30 @@
 import React from 'react'
 import {useRouter} from "next/router"
+import Link from 'next/link';
 
 const article = () => {
-    const router= useRouter();
-    const {id} = router.query
+    // const router= useRouter();
+    // const {id} = router.query
   return (
     <div>
-        <p>this is  article {id}</p>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br></br>
+        <Link href='/'>Go back</Link>
+       
     </div>
   )
 }
 
-export default article
+export default article;
+
+
+ export const getServerSideProps= async( context)=>{
+
+    const res=await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+    const article=await res.json();
+    return {props:{
+        article
+    }}
+
+ }
