66f134cee1b7b763d1b7228dbfb85e274469f0ee
ismayito <47735113+ismayito@users.noreply.github.com>
2022-10-11 13:12:26 +0300

    Adding a layout to the root function component

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import React, { Children } from 'react';
+
+import styles   from "../styles/Layout.module.css"
+
+const Layout = ({children}) => {
+  return (
+    <div className='styles.container'>
+        <main className='styles.main'>
+            <h1>header must go here</h1>
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default Layout
