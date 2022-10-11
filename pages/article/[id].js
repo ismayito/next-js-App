97a17ee07474db7f135a36a735f53f5c462bf617
ismayito <47735113+ismayito@users.noreply.github.com>
2022-10-11 16:57:55 +0300

    using getStaticPaths

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useRouter} from "next/router"
 import Link from 'next/link';
 
-const article = () => {
+const article = ({article}) => {
     // const router= useRouter();
     // const {id} = router.query
   return (
@@ -17,9 +17,29 @@ const article = () => {
 }
 
 export default article;
+ 
+
+export const getStaticPaths= async()=>{
+    
+    const res=await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const articles = await res.json();
+    const ids= articles.map(article =>(article.id));
+     const paths = ids.map(id=>({params:{id:id.toString()}}))
 
 
- export const getServerSideProps= async( context)=>{
+    return{
+       
+               paths,
+               fallback:false,
+            }
+        
+
+    
+
+ }
+
+
+ export const getStaticProps= async( context)=>{
 
     const res=await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
     const article=await res.json();
@@ -28,3 +48,12 @@ export default article;
     }}
 
  }
+//  export const getServerSideProps= async( context)=>{
+
+//     const res=await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+//     const article=await res.json();
+//     return {props:{
+//         article
+//     }}
+
+//  }
